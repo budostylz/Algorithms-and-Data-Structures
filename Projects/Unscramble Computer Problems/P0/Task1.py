d34c8b9141583816a509197c834117f387112f92
Shaun Lewis <lewis.shaun1@outlook.com>
2019-08-25 13:48:15 -0400

    Update Task1.py

--- Projects/Unscramble Computer Problems/P0/Task1.py
+++ Projects/Unscramble Computer Problems/P0/Task1.py
@@ -1,31 +1,3 @@
-"""
-Read file into texts and calls.
-It's ok if you don't understand how to read files.
-"""
-import csv
-
-texts = ''
-calls = ''
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-   
-
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
-
-
-print('texts', texts)
-print('calls', calls)
-
-"""
-TASK 1:
-How many different telephone numbers are there in the records? 
-Print a message:
-"There are <count> different telephone numbers in the records."
-"""
-
 """DESIGN
 Outputs:
   1. Count of different types of telephone numbers in both texts and calls
