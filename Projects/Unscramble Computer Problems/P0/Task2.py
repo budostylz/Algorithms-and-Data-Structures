e613a7717ffb9d50a7ac9333baf21d09ec05a6e5
budostylz <lewis.shaun1@outlook.com>
2019-08-26 21:02:41 -0400

    'unscramble_computer_problems'

--- Projects/Unscramble Computer Problems/P0/Task2.py
+++ Projects/Unscramble Computer Problems/P0/Task2.py
@@ -64,10 +64,7 @@ def getHighTime(sortedSeptemberList):
     return answeringNumber + " spent the longest time, "+totalTime+" seconds, on the phone during September 2016."
 
 
-septemberList = getSeptemberDates(calls)
-
-
-#Applied Bubble Sort function from Udacity Sorting Algorithm Section
+#Apply Bubble Sort pattern from Udacity Sorting Algorithm Section
 def septemberSort(septemberList):
     for iteration in range(len(septemberList)):
         for index in range(1, len(septemberList)):
@@ -82,8 +79,45 @@ def septemberSort(septemberList):
 
     return septemberList
 
-print('Sorting September List Using Bubble Sort...')
-sortedSeptemberList = septemberSort(septemberList)
-print(getHighTime(sortedSeptemberList))
+
+
+#Apply QuickSort pattern from Udacity Sorting Algorithm Section
+def sort_a_little_bit(items, begin_index, end_index):    
+    left_index = begin_index
+    pivot_index = end_index
+    pivot_value = int(items[pivot_index][3])
+
+    while (pivot_index != left_index):
+
+        item = int(items[left_index][3])
+        itemCache = items[left_index]
+
+        if item <= pivot_value:
+            left_index += 1
+            continue
+
+        items[left_index] = items[pivot_index - 1]
+        items[pivot_index - 1] = items[pivot_index]
+        items[pivot_index] = itemCache
+        pivot_index -= 1
+    
+    return pivot_index
+
+def sort_all(items, begin_index, end_index):
+    if end_index <= begin_index:
+        return
+    
+    pivot_index = sort_a_little_bit(items, begin_index, end_index)
+    sort_all(items, begin_index, pivot_index - 1)
+    sort_all(items, pivot_index + 1, end_index)
+    
+def quicksort(items):
+    sort_all(items, 0, len(items) - 1)
+    
+septemberList = getSeptemberDates(calls)
+quicksort(septemberList)
+print(getHighTime(septemberList))
+
+
 
 
